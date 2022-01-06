6e5b0d1c636175a7df6f21f2b089dd94e7dab4b2
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-01-06 19:59:23 +0400

    remove request headers

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from settings.urls_config import TUTORTOP_MAIN_PAGE
-from tutortop_parser.tutortop_parser import load_courses_data_by_category_url, load_main_page_course_links, write_data
+from tasks import run_tutortop_parser
 
 if __name__ == '__main__':
-    courses_category_links = load_main_page_course_links(TUTORTOP_MAIN_PAGE)
-    for category, url in courses_category_links.items():
-        data = load_courses_data_by_category_url(url, category)
-        write_data(data)
+    run_tutortop_parser()
